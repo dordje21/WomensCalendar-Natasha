de30ecd087538dbd7fc8ad393b9eca6e8f58010f
Aleksis Kvjatkovskis <107629187+AlexisKv@users.noreply.github.com>
2023-10-01 22:35:19 +0300

    Update questionnaire in App.jsx
    The questionnaire data in App.jsx has been updated with more relevant information. The previous generic questionnaire has been replaced with specifically framed questions and options to cater to a pregnancy-related app. Extra options have been added to accommodate multiple user responses. Moreover, one more question has been introduced to make the questionnaire more comprehensive. The formatting and functionality remain the same.

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,61 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import RadioButton from './components/RadioButton'
 
 function App() {
-  const [count, setCount] = useState(0);
-  
-  const questionsData = [
-    {
-      id: 1,
-      question: "Question 1?",
-      options: {
-        variant1: "Option 1",
-        variant2: "Option 2"
-      }
-    },
-    {
-      id: 2,
-      question: "Question 2?",
-      options: {
-        variant1: "Option 1",
-        variant2: "Option 2"
-      }
-    }
-  ];
+    const [count, setCount] = useState(0);
 
-  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-  const [selectedValues, setSelectedValues] = useState({});
+    const questionsData = [
+        {
+            id: 1,
+            question: "Are you pregnant??",
+            options: {
+                variant1: "No, but want to be",
+                variant2: "No, I'm here to understand my body better",
+                variant3: "Yes, I am"
+            }
+        },
+        {
+            id: 2,
+            question: "Is your menstrual cycle regular \n (varies by no more than 7 days) ?",
+            options: {
+                variant1: "My cycle is regular",
+                variant2: "My cycle is irregular",
+                variant3: "I don't know"
+            }
+        },
+        {
+            id: 3,
+            question: "When you was born?",
+            options: {
+                variant1: "Calendar"
+            }
+        }
+    ];
 
-  const handleRadioChange = (value) => {
-    setSelectedValues({ ...selectedValues, [currentQuestionIndex]: value });
-    setCurrentQuestionIndex(currentQuestionIndex + 1);
-  };
+    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+    const [selectedValues, setSelectedValues] = useState({});
 
-  const currentQuestion = questionsData[currentQuestionIndex];
+    const handleRadioChange = (value) => {
+        setSelectedValues({...selectedValues, [currentQuestionIndex]: value});
+        setCurrentQuestionIndex(currentQuestionIndex + 1);
+    };
 
-  return (
-    <div className="app-wrapper">
-      {currentQuestion && (
-        <div className="question">
-          <RadioButton
-            question={currentQuestion.question}
-            options={currentQuestion.options}
-            selectedValue={selectedValues[currentQuestionIndex]}
-            onChangeValue={(e) => handleRadioChange(e.target.value)}
-          />
+    const currentQuestion = questionsData[currentQuestionIndex];
+
+    return (
+        <div className="app-wrapper">
+            {currentQuestion && (
+                <div className="question">
+                    <RadioButton
+                        question={currentQuestion.question}
+                        options={currentQuestion.options}
+                        selectedValue={selectedValues[currentQuestionIndex]}
+                        onChangeValue={(e) => handleRadioChange(e.target.value)}
+                    />
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 }
 
 export default App;
