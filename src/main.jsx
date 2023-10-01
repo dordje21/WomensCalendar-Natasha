d5b2248ce4dc358253e107cb95dcfb95d16ef0b2
MykhailoLytvyn <cresedel211194@gmail.com>
2023-10-01 21:20:38 +0200

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 
 import { default as WebApp, default as WebAppInitData } from '@twa-dev/sdk'
 import React from 'react'
-import ReactDOM from 'react-dom/client.js'
+import ReactDOM from 'react-dom'
 import App from './App.jsx'
 import './index.css'
 
