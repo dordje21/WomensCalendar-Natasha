2f5f36e47a2f60995b8feb5f7d01a48f0d260002
Aleksis Kvjatkovskis <107629187+AlexisKv@users.noreply.github.com>
2023-10-04 01:30:37 +0300

    Update Start component to fetch user data
    Start component was updated, with user data fetch now initializing at the beginning of the app lifecycle. This is to ensure user data is available throughout the application. Previously, the user data was passed as a prop, but this made data handling less scalable and harder to manage. Now, a useEffect hook fetches user data from WebApp SDK at the component's first render. Moreover, 'user.is_premium' status has been added to the render to display premium status

--- src/components/Start/Start.jsx
+++ src/components/Start/Start.jsx
@@ -1,11 +1,17 @@
-import React, { useState } from 'react'
+import { default as WebApp } from '@twa-dev/sdk'
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from "react-router-dom"
 import RadioButton from '../RadioButton'
 
-function Start({user}) {	
+function Start() {	
 	const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
 	const [selectedValues, setSelectedValues] = useState({});
 
+	const [user, setUser] = useState('')
+
+  // useEffect(() => {
+  //   setUser(WebApp.initDataUnsafe.user)
+  // })
 
 	const navigate = useNavigate();
 
@@ -59,9 +65,11 @@ const currentQuestion = questionsData[currentQuestionIndex];
   return (
 		<>
 			<div>Start</div>
+			<p>{user.id}</p>
       <p>{user.first_name}</p>
       <p>{user.last_name}</p>
       <p>{user.username}</p>
+      <p>{user.is_premium}</p>
 								{currentQuestion && (
 									<div className="question">
 											<RadioButton
