d77f1fc128209585cbbce4109495dd6fa4b74077
Aleksis Kvjatkovskis <107629187+AlexisKv@users.noreply.github.com>
2023-10-04 00:24:25 +0300

    Update asset references and add content security policy
    Updated the script and stylesheet references in docs/index.html with the latest version. Additionally, a new meta tag for Content-Security-Policy was introduced. This is to strengthen the security by restricting the resources Chrome can load for the page to only those from the same origin and github.com.

--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,22 @@
+import { HashRouter } from 'react-router-dom';
+import { useRoutes } from 'react-router-dom';
+import Home from './components/Home/Home.jsx';
+import Start from './components/Start/Start.jsx';
+import ReactDOM from 'react-dom';
+import React from 'react';
 
-import { default as WebApp, default as WebAppInitData } from '@twa-dev/sdk'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {
-  RouterProvider,
-  createBrowserRouter,
-} from "react-router-dom"
-import Home from './components/Home/Home.jsx'
-import Start from './components/Start/Start.jsx'
-import './index.css'
+function App() {
+  let element = useRoutes([
+    { path: '/', element: <Start /> },
+    { path: '/home', element: <Home /> },
+  ]);
 
-WebApp.ready();
-WebAppInitData.ready();
+  return element;
+}
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Start />,
-  },
-  {
-    path: "/home",
-    element: <Home />,
-  }
-]);
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+ReactDOM.render(
+    <HashRouter>
+      <App />
+    </HashRouter>,
+    document.getElementById('root')
 );
