ffc6c1c0ded25f6eca83fe973e4ed680dac89e56
MykhailoLytvyn <cresedel211194@gmail.com>
2023-10-04 20:46:05 +0200

    custom routs

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { default as WebApp, default as WebAppInitData } from '@twa-dev/sdk'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { HashRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
@@ -9,8 +8,6 @@ WebApp.ready();
 WebAppInitData.ready();
 
 ReactDOM.render(
-  <HashRouter>
-    <App />
-  </HashRouter>,
+  <App />,
   document.getElementById('root')
 );
