3b248ab9d5d135bf57fb3ac5e5822a8305696e58
Aleksis Kvjatkovskis <107629187+AlexisKv@users.noreply.github.com>
2023-10-04 03:20:32 +0300

    Refactor Start.jsx and add new features
    Start.jsx was refactored to improve data organization and date management. Two new components, CalendarMi and CalendarPrediction, were added to enhance date tracking and prediction functions. Variable names were adjusted for better readability. Comments were left for potentially useful but unused code blocks for future reference. A new file, Ssstart.jsx, was also created as a fallback version of the original Start file.

--- src/components/Start/Start.jsx
+++ src/components/Start/Start.jsx
@@ -2,86 +2,93 @@ import {default as WebApp} from '@twa-dev/sdk'
 import React, {useEffect, useState} from 'react'
 import {useNavigate} from "react-router-dom"
 import RadioButton from '../RadioButton'
+import CalendarMi from "../Calendar.jsx";
+import CalendarPrediction from "../CalendarPrediction.jsx";
 
 function Start({user}) {
-    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-    const [selectedValues, setSelectedValues] = useState({});
+    const [count, setCount] = useState(0);
+    // const [data, setData] = useState({});
 
-    // const [user, setUser] = useState('')
+    const [date, setDate] = useState(new Date());
+    // const [user, setUser] = useState('');
+    // useEffect(() => {
+    //     console.log(selectedValues)
+    // },[selectedValues])
 
     // useEffect(() => {
-    //   setUser(WebApp.initDataUnsafe.user)
-    // })
+    //     async function getData(db) {
+    //         const collectionRef = collection(db, 'test');
+    //         const citySnapshot = await getDocs(collectionRef);
+    //         const cityList = citySnapshot.docs.map(doc => doc.data());
+    //         return cityList;
+    //     }
 
-    const navigate = useNavigate();
+    //   async function getAllDocuments() {
+    //       try {
+    //           const req = await getData(db);
+    //           console.log(req)
+    //           setData(req)
+    //       } catch (error) {
+    //           console.error('Error getting documents:', error);
+    //       }
+    //   }
 
-    const questionsData = [
-        {
-            id: 1,
-            question: "Are you pregnant??",
-            options: {
-                variant1: "No, but want to be",
-                variant2: "No, I'm here to understand my body better",
-                variant3: "Yes, I am"
-            }
-        },
-        {
-            id: 2,
-            question: "Is your menstrual cycle regular \n (varies by no more than 7 days) ?",
-            options: {
-                variant1: "My cycle is regular",
-                variant2: "My cycle is irregular",
-                variant3: "I don't know"
-            }
-        },
-        {
-            id: 3,
-            question: "How many days on average is your cycle?",
-            options: {
-                variant1: "Max 56 min 21 day options"
-            }
-        },
-        {
-            id: 4,
-            question: "How many days on average is your menstruation?",
-            options: {
-                variant1: "Max 12 min 1 day options"
-            }
-        }
-    ];
+    //   getAllDocuments();
 
 
-    const handleRadioChange = (value) => {
-        setSelectedValues({...selectedValues, [currentQuestionIndex]: value});
-        setCurrentQuestionIndex(currentQuestionIndex + 1);
-        if (questionsData.length <= currentQuestionIndex + 1) {
-            navigate("/home");
-        }
-        console.log(questionsData.length, currentQuestionIndex + 1)
-    };
+    //   const setDataInDb = async (value) => {
+    //     try {
+    //       const collectionRef = collection(db, 'test');
+    //       await addDoc(collectionRef, { test: value });
+    //       console.log('Data added to Firestore');
+    //     } catch (error) {
+    //       console.error('Error adding data to Firestore:', error);
+    //     }
+    //   };
 
-    const currentQuestion = questionsData[currentQuestionIndex];
+    //     setDataInDb('Alex')
+    // }, []);
+
+
+
+
+
+
+    let daysFirstLetter = ["S", "M", "T", "W", "T", "F", "S"];
+
+    function GetDaysFirstLetter(date) {
+        return daysFirstLetter[date.getDay()];
+    }
+
+    let todayDate = new Date();
+    let minimalDate = -2
+    let maximalDate = 4
+    let dates = [];
+    for (let i = minimalDate; i <= maximalDate; i++) {
+        let date = new Date();
+        date.setDate(date.getDate() + i);
+        dates.push(date);
+    }
 
     return (
-        <>
+        <div className="app-wrapper">
+            <div style={{display: 'flex', justifyContent: 'space-around'}}>
+                {dates.map((date, index) => (
+                    <div key={index} style={{margin: '0 20px'}}>
+                        <h2>{date.toDateString() === todayDate.toDateString() ? 'Today' : GetDaysFirstLetter(date)}</h2>
+                        <h2>{date.getDate()}</h2>
+                    </div>
+                ))}
+            </div>
             <div>Start</div>
             <p>{user?.id}</p>
             <p>{user?.first_name}</p>
             <p>{user?.last_name}</p>
             <p>{user?.username}</p>
             <p>{user?.is_premium}</p>
-            {/*{currentQuestion && (*/}
-            {/*    <div className="question">*/}
-            {/*        <RadioButton*/}
-            {/*            question={currentQuestion.question}*/}
-            {/*            options={currentQuestion.options}*/}
-            {/*            selectedValue={selectedValues[currentQuestionIndex]}*/}
-            {/*            onChangeValue={(e) => handleRadioChange(e.target.value)}*/}
-            {/*        />*/}
-            {/*    </div>*/}
-            {/*)}*/}
-        </>
+            <CalendarMi/>
+            <CalendarPrediction/>
+        </div>
     );
 }
-
 export default Start;
