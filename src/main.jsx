ab6cc9d86a37ee1b518e5cb3500d8c96774657c3
Aleksis Kvjatkovskis <107629187+AlexisKv@users.noreply.github.com>
2023-10-07 19:07:51 +0300

    a' holds the keys and adds it in the render method of the React DOM.

--- src/main.jsx
+++ src/main.jsx
@@ -8,11 +8,10 @@ import './index.css'
 WebApp.ready();
 WebAppInitData.ready();
 WebApp.CloudStorage.setItem("Test", "2");
-let a = WebApp.CloudStorage.getKeys();
+WebApp.CloudStorage.getKeys();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <>{a}</>
         <App />
     </React.StrictMode>,
 )
