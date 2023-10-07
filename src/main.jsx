842eb33addb2b7b2f0e6add06b8477a04288dcd4
Aleksis Kvjatkovskis <107629187+AlexisKv@users.noreply.github.com>
2023-10-07 20:15:19 +0300

    Remove duplicate import and fix usage of CloudStorage
    Removed unnecessary import of CloudStorage as it was already imported as a part of WebApp from '@twa-dev/sdk'. Also, corrected the usage of getItems() to getItem() as we need to fetch a single item from storage.

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
 import {default as WebApp, default as WebAppInitData} from '@twa-dev/sdk'
-import CloudStorage from '@twa-dev/sdk'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
@@ -11,10 +10,9 @@ WebApp.CloudStorage.setItem("Test", "2");
 WebApp.CloudStorage.setItem("Test", "221");
 WebApp.CloudStorage.setItem("Testas", "1");
 
-
 WebApp.CloudStorage.getKeys();
 WebApp.CloudStorage.getItems("Test");
-
+WebApp.CloudStorage.getItem("Test");
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
