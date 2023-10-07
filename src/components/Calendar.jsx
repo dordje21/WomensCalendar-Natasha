51abc70a0ed66e72a2b90c7990a17755ec4eda6f
MykhailoLytvyn <cresedel211194@gmail.com>
2023-10-08 01:04:08 +0200

    tg db fix

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -2,15 +2,19 @@ import { Calendar } from '@bjarkehs/react-nice-dates'
 import '@bjarkehs/react-nice-dates/build/style.css'
 import { getDay, isSameDay } from 'date-fns'
 import { enGB } from 'date-fns/locale'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 // Very rough implementation of multiple date selection
 export default function CalendarMi({nextDate}) {
   const [selectedDates, setSelectedDates] = useState([])
   const modifiers = {
     selected: date => selectedDates.some(selectedDate => isSameDay(selectedDate, date)),
-		disabled: date => getDay(date) === nextDate, 
-		highlight: date => getDay(date) === 2 
+		disabled: date => getDay(date) === 1, 
+		highlight: date => getDay(date) === 6
   }
+
+	useEffect(()=>{
+		setSelectedDates([...selectedDates, nextDate])
+	})
 
 	const modifiersClassNames = {
 		highlight: '-highlight'
