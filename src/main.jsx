f7439f8d39230f704291f4da8e27f3c851e29100
Aleksis Kvjatkovskis <107629187+AlexisKv@users.noreply.github.com>
2023-10-07 19:05:08 +0300

    Add CloudStorage keys to React root
    This commit includes the retrieval of keys from CloudStorage and their inclusion to the React root. The change was done to visualize

--- src/main.jsx
+++ src/main.jsx
@@ -8,9 +8,11 @@ import './index.css'
 WebApp.ready();
 WebAppInitData.ready();
 WebApp.CloudStorage.setItem("Test", "2");
+let a = WebApp.CloudStorage.getKeys();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
+        <>{a}</>
         <App />
     </React.StrictMode>,
 )
