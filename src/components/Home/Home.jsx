3a90cd9e957680d04a3c5f2855afe2146aab2087
Aleksis Kvjatkovskis <107629187+AlexisKv@users.noreply.github.com>
2023-10-10 14:57:27 +0300

    d

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -198,7 +198,7 @@ function Home({user, handleReset}) {
         const [todayWithoutTime, nextWithoutTime] = [todayDate, nextDate].map(resetTime);
 
         const timeDifference = nextWithoutTime - todayWithoutTime 
-        const daysDifference = Math.ceil(timeDifference / (1000 * 60 * 60 * 24)) ;
+        const daysDifference = Math.ceil(timeDifference / (1000 * 60 * 60 * 24)) - 1 ;
         console.log("TodayDate :"+ todayDate+" NextDate :"+ nextDate+" DaysDifference: "+daysDifference)
         return daysDifference
     }
